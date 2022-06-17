67f58b8ab4efd99c38338c4143e1a62283831524
KaitmazianMO <kaitmazian.mo@phystech.edu>
2022-06-17 23:02:04 +0300

    Algorithm I: Solve the 13th day

---
+++ Algorithm-I/Day-13/power_of_two.cpp
@@ -0,0 +1,40 @@
+/*
+ * Given an integer n, return true if it is a power of two. Otherwise, return false.
+ *
+ * An integer n is a power of two, if there exists an integer x such that n == 2x.
+ *
+ * Example 1:
+ *
+ * Input: n = 1
+ * Output: true
+ * Explanation: 20 = 1
+ * Example 2:
+ *
+ * Input: n = 16
+ * Output: true
+ * Explanation: 24 = 16
+ * Example 3:
+ *
+ * Input: n = 3
+ * Output: false
+ */
+
+#include <cassert>
+
+class Solution {
+public:
+        bool isPowerOfTwo(int n) {
+                return (n > 0) && ((n & (n - 1)) == 0);
+        }
+};
+
+int main() {
+        Solution solution;
+        assert(solution.isPowerOfTwo(1));
+        assert(solution.isPowerOfTwo(2));
+        assert(solution.isPowerOfTwo(1<<16));
+        assert(!solution.isPowerOfTwo(0));
+        assert(!solution.isPowerOfTwo(3));
+        assert(!solution.isPowerOfTwo(1023));
+        return 0;
+}
