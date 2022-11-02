84f10609ba955eac119014a04c14ecd186a63615
KaitmazianMO <kaitmazian.mo@phystech.edu>
2022-11-02 22:59:47 +0300

    Algorithm-I/refactor: apply STL containers

--- Algorithm-I/Day-6/longest_substring_without_repeating_characters.cpp
+++ Algorithm-I/Day-6/longest_substring_without_repeating_characters.cpp
@@ -1,25 +1,25 @@
 /*
  * Given a string s, find the length of the longest substring without repeating characters.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: s = "abcabcbb"
  * Output: 3
  * Explanation: The answer is "abc", with the length of 3.
  * Example 2:
- * 
+ *
  * Input: s = "bbbbb"
  * Output: 1
  * Explanation: The answer is "b", with the length of 1.
  * Example 3:
- * 
+ *
  * Input: s = "pwwkew"
  * Output: 3
  * Explanation: The answer is "wke", with the length of 3.
  * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
- *  
+ *
  * Constraints:
- * 
+ *
  * 0 <= s.length <= 5 * 104
  * s consists of English letters, digits, symbols and spaces.
  */
@@ -29,49 +29,42 @@
 #include <string>
 #include <cstring>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-        struct char_info {
-                int quantity;
-                int position;
-        };
+	int lengthOfLongestSubstring(string s) {
+		unordered_map<char, size_t> positions;
+		size_t max_length = 0;
+		size_t length = 0;
 
-        int lengthOfLongestSubstring(string s) {
-                const int n_chars = 256;
-                char_info chars[n_chars] = {};
+		for (size_t i = 0, sz = s.size(); i < sz; ++i) {
+			if (positions.find(s[i]) == positions.end()) {
+				positions[s[i]] = i;
+				length += 1;
+				max_length = max(max_length, length);
+			} else {
+				size_t duplicate_lpos = positions[s[i]];
+				for (size_t j = i - length; j < duplicate_lpos; j++)
+					positions.erase(s[j]);
+				length = i - duplicate_lpos;
+				positions[s[i]] = i;
+			}
+		}
 
-                int i = 0;
-                int max_lenght = 0;
-                int cur_lenght = 0;
-                const int sz = s.size();
-                while (i < sz) {
-                        int idx = s[i];
-                        if (chars[idx].quantity < 1) {
-                                chars[idx].quantity = 1;
-                                chars[idx].position = i;
-                                max_lenght = max(max_lenght, ++cur_lenght);
-                                ++i;
-                        } else {
-                                int new_pos = chars[idx].position + 1;
-                                for (int j = i - cur_lenght; j <= chars[idx].position; ++j)
-                                        chars[(int)s[j]] = {};
-                                cur_lenght = i - new_pos;
-                        }
-                }
-                return max_lenght;
-        }
+		return max_length;
+	}
 };
 
 int main() {
-        Solution solution;
-        assert(solution.lengthOfLongestSubstring("abcder") == 6);
-        assert(solution.lengthOfLongestSubstring("abcabcbb") == 3);
-        assert(solution.lengthOfLongestSubstring("abcdbcbb") == 4);
-        assert(solution.lengthOfLongestSubstring("ababc") == 3);
-        assert(solution.lengthOfLongestSubstring("abcdabcc") == 4);
-        assert(solution.lengthOfLongestSubstring("abcabcbb") == 3);
-        return 0;
+	Solution solution;
+	assert(solution.lengthOfLongestSubstring("abcder") == 6);
+	assert(solution.lengthOfLongestSubstring("abcabcbb") == 3);
+	assert(solution.lengthOfLongestSubstring("abcdbcbb") == 4);
+	assert(solution.lengthOfLongestSubstring("ababc") == 3);
+	assert(solution.lengthOfLongestSubstring("abcdabcc") == 4);
+	assert(solution.lengthOfLongestSubstring("abcabcbb") == 3);
+	return 0;
 }
--- Algorithm-I/Day-6/permutation_in_string.cpp
+++ Algorithm-I/Day-6/permutation_in_string.cpp
@@ -1,21 +1,21 @@
 /*
  * Given two strings s1 and s2, return true if s2
  * contains a permutation of s1, or false otherwise.
- * 
+ *
  * In other words, return true if one of s1's permutations is the substring of s2.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: s1 = "ab", s2 = "eidbaooo"
  * Output: true
  * Explanation: s2 contains one permutation of s1 ("ba").
  * Example 2:
- * 
+ *
  * Input: s1 = "ab", s2 = "eidboaoo"
  * Output: false
- *  
+ *
  * Constraints:
- * 
+ *
  * 1 <= s1.length, s2.length <= 104
  * s1 and s2 consist of lowercase English letters.
  */
@@ -23,42 +23,40 @@
 #include <cassert>
 #include <string>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-        bool checkInclusion(string s1, string s2) {
-                const int n_letters = 'z' - 'a' + 1;
-                int storage1[n_letters] = {};
-                int storage2[n_letters] = {};
-                const int sz1 = s1.size();
-                const int sz2 = s2.size();
-                if (sz2 < sz1) {
-                        return false;
-                }
-                for (int i = 0; i < sz1; ++i) {
-                        ++storage1[s1[i] - 'a'];
-                        ++storage2[s2[i] - 'a'];
-                }
-                
-                for (int i = 0; i < sz2 - sz1; ++i) {
-                        if (equal(storage1, storage1 + n_letters, storage2))
-                                return true;
-                        --storage2[s2[i] - 'a'];
-                        ++storage2[s2[i + sz1] - 'a'];
-                }
-                return equal(storage1, storage1 + n_letters, storage2);
-        }
+
+	bool checkInclusion(string s1, string s2) {
+		const size_t sz1 = s1.size();
+		const size_t sz2 = s2.size();
+
+		vector<size_t> letters1('z' + 1);
+		vector<size_t> letters2('z' + 1);
+		for (size_t i = 0; i < sz1; ++i) {
+			letters1[s1[i]] += 1;
+			letters2[s2[i]] += 1;
+		}
+
+		for (size_t i = 0; i < sz2 - sz1; ++i) {
+			if (letters1 == letters2)
+				return true;
+			letters2[s2[i + sz1]] += 1;
+			letters2[s2[i]] -= 1;
+		}
+		return letters1 == letters2;
+	}
 };
 
 int main() {
-        Solution solution;
-        assert(solution.checkInclusion("ab", "ab"));
-        assert(solution.checkInclusion("ab", "ba"));
-        assert(!solution.checkInclusion("abc", "ba"));
-        assert(solution.checkInclusion("abc", "bac"));
-        assert(solution.checkInclusion("ac", "bac"));
-        assert(solution.checkInclusion("ac", "bdwaszxaaadcadsaw"));
-        return 0;
+	Solution solution;
+	assert(solution.checkInclusion("ab", "ab"));
+	assert(solution.checkInclusion("ab", "ba"));
+	assert(solution.checkInclusion("abc", "bac"));
+	assert(solution.checkInclusion("ac", "bac"));
+	assert(solution.checkInclusion("ac", "bdwaszxaaadcadsaw"));
+	return 0;
 }
